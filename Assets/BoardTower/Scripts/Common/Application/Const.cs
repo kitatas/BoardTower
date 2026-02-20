b13c414915490439ccd0f49251902bbf404fe785
kitatas <315kitatas10703@gmail.com>
2026-02-21 04:26:46 +0900

    add: ExceptionVO

---
+++ Assets/BoardTower/Scripts/Common/Application/Const.cs
@@ -0,0 +1,9 @@
+namespace BoardTower.Common.Application
+{
+    public sealed class ExceptionConfig
+    {
+        public const string REBOOT_MESSAGE = "Return to title.";
+        public const string RETRY_MESSAGE = "Retry.";
+        public const string QUIT_MESSAGE = "Exit this app.";
+    }
+}
--- Assets/BoardTower/Scripts/Common/Application/ValueObject.cs
+++ Assets/BoardTower/Scripts/Common/Application/ValueObject.cs
@@ -1,5 +1,43 @@
+using System;
+using Cysharp.Text;
+
 namespace BoardTower.Common.Application
 {
+    public abstract class ExceptionVO : Exception
+    {
+        public ExceptionVO(string message) : base(message)
+        {
+        }
+
+        public virtual string exceptionMessage => ExceptionConfig.QUIT_MESSAGE;
+        public string message => ZString.Format("{0}\n{1}", base.Message, exceptionMessage);
+    }
+
+    public sealed class RebootExceptionVO : ExceptionVO
+    {
+        public RebootExceptionVO(string message) : base(message)
+        {
+        }
+
+        public override string exceptionMessage => ExceptionConfig.REBOOT_MESSAGE;
+    }
+
+    public sealed class RetryExceptionVO : ExceptionVO
+    {
+        public RetryExceptionVO(string message) : base(message)
+        {
+        }
+
+        public override string exceptionMessage => ExceptionConfig.RETRY_MESSAGE;
+    }
+
+    public sealed class QuitExceptionVO : ExceptionVO
+    {
+        public QuitExceptionVO(string message) : base(message)
+        {
+        }
+    }
+
     public abstract class TransitionVO
     {
         public readonly Fade fade;
