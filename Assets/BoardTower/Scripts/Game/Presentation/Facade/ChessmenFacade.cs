189724ad3fe28071d4902a79a09807fa996580ba
kitatas <315kitatas10703@gmail.com>
2026-02-21 03:37:08 +0900

    add: ChessmenFacade

---
+++ Assets/BoardTower/Scripts/Game/Presentation/Facade/ChessmenFacade.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using BoardTower.Common.Application;
+using BoardTower.Game.Application;
+using BoardTower.Game.Presentation.View;
+using Cysharp.Threading.Tasks;
+
+namespace BoardTower.Game.Presentation.Facade
+{
+    public sealed class ChessmenFacade
+    {
+        private readonly ChessmenView _chessmenView;
+
+        public ChessmenFacade(ChessmenView chessmenView)
+        {
+            _chessmenView = chessmenView;
+        }
+
+        public UniTask FadeAsync(ChessmenTransitionVO transition, CancellationToken token)
+        {
+            var tween = transition.fade switch
+            {
+                Fade.In => _chessmenView.FadeIn(transition.duration),
+                Fade.Out => _chessmenView.FadeOut(transition.duration),
+                _ => throw new Exception(), // TODO: Exception
+            };
+
+            return tween
+                .ToUniTask(TweenCancelBehaviour.KillAndCancelAwait, token);
+        }
+    }
+}
