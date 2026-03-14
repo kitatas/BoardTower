ffd9e71958f5a22596141a2790a7161251cf3713
kitatas <315kitatas10703@gmail.com>
2026-03-14 16:45:54 +0900

    fix: Board / ChessmenのFadeOut

--- Assets/BoardTower/Scripts/Game/Presentation/State/GameJudgeState.cs
+++ Assets/BoardTower/Scripts/Game/Presentation/State/GameJudgeState.cs
@@ -1,18 +1,32 @@
 using System.Threading;
+using BoardTower.Common.Application;
 using BoardTower.Game.Application;
+using BoardTower.Game.Domain.UseCase;
 using Cysharp.Threading.Tasks;
 
 namespace BoardTower.Game.Presentation.State
 {
     public sealed class GameJudgeState : BaseGameState
     {
+        private readonly BoardUseCase _boardUseCase;
+        private readonly ChessmenUseCase _chessmenUseCase;
+
+        public GameJudgeState(BoardUseCase boardUseCase, ChessmenUseCase chessmenUseCase)
+        {
+            _boardUseCase = boardUseCase;
+            _chessmenUseCase = chessmenUseCase;
+        }
+
         public override GameState state => GameState.Judge;
 
         public override async UniTask<GameState> TickAsync(CancellationToken token)
         {
             // TODO: round目標達成しているか
             {
-                await UniTask.Yield(PlayerLoopTiming.Update, token);
+                await (
+                    _boardUseCase.FadeAsync(Fade.Out, token),
+                    _chessmenUseCase.FadeAsync(Fade.Out, token)
+                );
                 return GameState.SetUp;
             }
         }
