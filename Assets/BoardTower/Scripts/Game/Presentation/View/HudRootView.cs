adc9d1db80798bb4acaaa5f0105d781cb3069eeb
kitatas <315kitatas10703@gmail.com>
2026-06-05 00:04:48 +0900

    fix: Fade HudRoot

--- Assets/BoardTower/Scripts/Game/Presentation/View/HudRootView.cs
+++ Assets/BoardTower/Scripts/Game/Presentation/View/HudRootView.cs
@@ -1,4 +1,3 @@
-using BoardTower.Game.Application;
 using DG.Tweening;
 using UnityEngine;
 
@@ -6,40 +5,46 @@ namespace BoardTower.Game.Presentation.View
 {
     public sealed class HudRootView : MonoBehaviour
     {
-        [SerializeField] private RectTransform[] huds = default;
+        [SerializeField] private RectTransform topSide = default;
+        [SerializeField] private RectTransform rightSide = default;
+        [SerializeField] private RectTransform leftSide = default;
 
         public Tween FadeIn(float duration)
         {
-            var sequence = DOTween.Sequence();
-            for (int i = 0; i < huds.Length; i++)
-            {
-                var x = Mathf.Abs(huds[i].anchoredPosition.x);
-                sequence
-                    .Join(huds[i]
-                        .DOAnchorPosX(x, duration)
-                        .SetEase(Ease.OutBack)
-                        .SetDelay(duration * i * HudRootConfig.FADE_DELAY_RATE))
-                    .SetLink(gameObject);
-            }
+            var topY = Mathf.Abs(topSide.anchoredPosition.y) * -1.0f;
+            var rightX = Mathf.Abs(rightSide.anchoredPosition.x) * -1.0f;
+            var leftX = Mathf.Abs(leftSide.anchoredPosition.x);
 
-            return sequence;
+            return DOTween.Sequence()
+                .Join(topSide
+                    .DOAnchorPosY(topY, duration)
+                    .SetEase(Ease.OutBack))
+                .Join(rightSide
+                    .DOAnchorPosX(rightX, duration)
+                    .SetEase(Ease.OutBack))
+                .Join(leftSide
+                    .DOAnchorPosX(leftX, duration)
+                    .SetEase(Ease.OutBack))
+                .SetLink(gameObject);
         }
 
         public Tween FadeOut(float duration)
         {
-            var sequence = DOTween.Sequence();
-            for (int i = 0; i < huds.Length; i++)
-            {
-                var x = Mathf.Abs(huds[i].anchoredPosition.x);
-                sequence
-                    .Join(huds[i]
-                        .DOAnchorPosX(x * -1.0f, duration)
-                        .SetEase(Ease.OutBack)
-                        .SetDelay(duration * i * HudRootConfig.FADE_DELAY_RATE))
-                    .SetLink(gameObject);
-            }
+            var topY = Mathf.Abs(topSide.anchoredPosition.y);
+            var rightX = Mathf.Abs(rightSide.anchoredPosition.x);
+            var leftX = Mathf.Abs(leftSide.anchoredPosition.x) * -1.0f;
 
-            return sequence;
+            return DOTween.Sequence()
+                .Join(topSide
+                    .DOAnchorPosY(topY, duration)
+                    .SetEase(Ease.OutBack))
+                .Join(rightSide
+                    .DOAnchorPosX(rightX, duration)
+                    .SetEase(Ease.OutBack))
+                .Join(leftSide
+                    .DOAnchorPosX(leftX, duration)
+                    .SetEase(Ease.OutBack))
+                .SetLink(gameObject);
         }
     }
 }
