f7f4b84f147a6b40ee662f35dd6a37d172198e8d
kitatas <315kitatas10703@gmail.com>
2026-07-20 18:47:13 +0900

    fix: namespace

--- Assets/BoardTower/Scripts/Common/Data/DataStore/PlayFabMasterDTO.cs
+++ Assets/BoardTower/Scripts/Common/Data/DataStore/PlayFabMasterDTO.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using BoardTower.Common.Application;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace BoardTower.Common.Data.DataStore
 {
